95f4c0192b619891cbef23f91a19fbf00b251546
Érico Rolim <erico.erc@gmail.com>
2020-09-12 23:43:03 -0300

    Add tests for clean_up_links.
    Clean up makefile.

--- makefile
+++ makefile
@@ -1,11 +1,12 @@
 CFLAGS = -O2 -g -pipe -Wall -Wextra
 LDLIBS = -lbearssl -lsbearssl -lskarnet
+LDFLAGS = -Wl,--as-needed
 
 FINAL = purr
 OBJS = purr.o socket.o urls.o files.o comm.o formats.o encrypt.o
 
 TEST = tests
-TOBJS = tests.o formats.o
+TOBJS = tests.o formats.o urls.o
 
 all: $(FINAL)
 
@@ -19,4 +20,4 @@ tests: $(TOBJS)
 $(OBJS): purr.h
 
 clean:
-	rm -f $(FINAL) $(OBJS)
+	rm -f $(FINAL) $(OBJS) $(TEST) $(TOBJS)
--- tests.c
+++ tests.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 #include "purr.h"
+
+static int compare_strings(const char *expected, const char *result, const char *function)
+{
+    int rv = 0;
+
+    printf("%s(): ", function);
+    if (strcmp(expected, result)) {
+        rv = 1;
+        puts("failure");
+        printf("expected: %s\ngot: %s\n", expected, result);
+    } else {
+        puts("success");
+    }
+
+    return rv;
+}
 
 int main()
 {
@@ -10,17 +27,27 @@ int main()
     {
         /* formats.c */
         uint8_t buf[] = {0x12, 0x02, 0x12, 0x4c, 0xa8};
-        char *expected = "1202124ca8";
-        char *result = print_hex(buf, sizeof buf, false);
+        const char *expected = "1202124ca8";
+        const char *result = print_hex(buf, sizeof buf, false);
+        rv = compare_strings(expected, result, "print_hex") ? 1 : rv;
+    }
 
-        printf("print_hex(): ");
-        if (strcmp(expected, result)) {
-            puts("failure");
-            printf("expected: %s\ngot: %s\n", expected, result);
-            rv = 1;
-        } else {
-            puts("success");
-        }
+    {
+        /* urls.c */
+        const char *dirty = "https://hello.com/ash";
+        char clean[4096], path[4096], port[16];
+        int portn = clean_up_link(dirty, clean, path, port);
+        rv = compare_strings("hello.com", clean, "clean_up_link") ? 1 : rv;
+        rv = compare_strings("/ash", path, "clean_up_link") ? 1 : rv;
+        rv = compare_strings("443", port, "clean_up_link") ? 1 : rv;
+        assert(portn == HTTPS_PORT);
+
+        dirty = "http://hello.com";
+        portn = clean_up_link(dirty, clean, path, port);
+        rv = compare_strings("hello.com", clean, "clean_up_link") ? 1 : rv;
+        rv = compare_strings("/", path, "clean_up_link") ? 1 : rv;
+        rv = compare_strings("80", port, "clean_up_link") ? 1 : rv;
+        assert(portn == HTTP_PORT);
     }
 
     return rv;
