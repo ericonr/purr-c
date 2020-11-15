072ac1f946da1a575b8444dd538ef10c330c7fb3
Érico Rolim <erico.erc@gmail.com>
2020-11-15 20:57:21 -0300

    Make gettext calls a noop when building with make.
    Since the Make build doesn't know how to install locale files, avoid
    using any locale/libintl functions for it.

--- translation.h
+++ translation.h
@@ -1,6 +1,8 @@
 #ifndef __TRANSLATION_H_
 #define __TRANSLATION_H_
 
+#ifdef USE_LIBINTL
+
 #include <locale.h>
 #include <libintl.h>
 #define _(a) gettext(a)
@@ -12,5 +14,14 @@
 #ifndef GETTEXT_DIR
 #define GETTEXT_DIR NULL
 #endif /* GETTEXT_DIR */
+
+#else /* USE_LIBINTL */
+
+#define _(a) a
+#define setlocale(a,b)
+#define bindtextdomain(a,b)
+#define textdomain(a)
+
+#endif /* USE_LIBINTL */
 
 #endif // __TRANSLATION_H_
